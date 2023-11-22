81772ad61b80f9aedd65aa5857f164adbfe7e44f
praveensrinivasan01 <praveensrinivasan01@gmail.com>
2023-11-22 13:13:27 +0530

    category

--- src/Pages/AdminLayout/Dashboard.jsx
+++ src/Pages/AdminLayout/Dashboard.jsx
@@ -63,7 +63,7 @@ const Dashboard = () => {
  transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
         </div>
         <div className="text-right">
-            <p className="text-2xl">${count?.total_earned}</p>
+                      <p className="text-2xl">₹{count?.total_earned}</p>
                       <p>Earnings</p>
         </div>
         </div>
